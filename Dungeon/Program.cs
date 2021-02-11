b8c74cb546f2226fe4b3739d6c32a0b34543e29a
mbell8464 <michaelb2127@charter.net>
2021-02-10 21:08:11 -0600

    Title and Intro to game

--- Dungeon/Program.cs
+++ Dungeon/Program.cs
@@ -10,6 +10,8 @@ namespace Dungeon
     {
         static void Main(string[] args)
         {
+            Console.Title = "Escape Asylum";
+            Console.WriteLine("You awake to find yourself lying on a cold wet stone slab. The room is black, it smells of death.\nYou have no idea where you are, but you know you must leave.\nYou begin to hear noises echoing in the distance. You must escape.\nGood luck...\n");
         }
     }
 }
